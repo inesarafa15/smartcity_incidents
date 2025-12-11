202ff8076278a3b1ef73aea04d8fa78cabc44d10
inesarafa15 <inesarafa15@gmail.com>
2025-12-11 02:43:59 +0100

    Initial commit

---
+++ src/main/java/com/city/incident_platform/IncidentPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.city.incident_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/city/incident_platform/IncidentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.city.incident_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
