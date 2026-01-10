c093fdca9e4bb7d3628864aebdf167f839d6634b
inesarafa15 <inesarafa15@gmail.com>
2026-01-10 04:14:19 +0100

    chore: ajout tests demonstration CI (fail)

---
+++ src/test/java/com/smartcity/incident_management/LogicFailTest.java
@@ -0,0 +1,18 @@
+package com.smartcity.incident_management;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LogicFailTest {
+
+    @Test
+    void intentionalFailure_ForCIDemo() {
+        // Given
+        int expected = 100;
+        int actual = 50 + 49; // Fait 99
+
+        // When & Then
+        // FIX: Changer 49 en 50 pour que le test passe
+        assertEquals(expected, actual, "ERREUR VOLONTAIRE : 99 n'est pas égal à 100. La CI doit échouer ici.");
+    }
+}
